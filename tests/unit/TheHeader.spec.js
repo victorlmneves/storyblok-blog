ecc4fa290456f570a28e1e329338040936b32bf1
Victor Neves <info@vitorneves.com>
2020-12-07 19:39:36 +0000

    Fixed issue on the tests related to NuxtLink

--- tests/unit/TheHeader.spec.js
+++ tests/unit/TheHeader.spec.js
@@ -3,7 +3,12 @@ import TheHeader from '@/components/Base/TheHeader.vue'
 
 describe("TheHeader", () => {
   test("is a Vue instance", () => {
-    const wrapper = mount(TheHeader);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
+    const wrapper = mount(TheHeader, {
+      stubs: {
+        NuxtLink: true
+        // Any other component that you want stubbed
+      }
+    })
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
