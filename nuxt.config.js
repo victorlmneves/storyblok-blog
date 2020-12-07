6383e4f282443adffbcc606491db73bad8836e8e
Victor Neves <info@vitorneves.com>
2020-12-07 19:03:30 +0000

    Added new storyblok-nuxt,  node-sass and sass-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,38 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  htmlAttrs: {
+    lang: "en"
+  },
   head: {
-    title: 'storyblok-blog',
+    title: "Storyblok blog + Nuxt.js",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "pkg.description" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+      }
     ]
   },
 
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#3b8070" },
+
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -29,19 +40,25 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    // "@nuxtjs/axios",
+    [
+      "storyblok-nuxt",
+      {
+        accessToken: "4PKdqSCTPjzgDYKNUK6opwtt",
+        cacheProvider: "memory"
+      }
+    ]
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};
